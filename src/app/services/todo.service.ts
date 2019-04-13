7669559be7deeaf66b04b2ebe2ce03be58ff9500
realbenfica <alexgtr84@gmail.com>
2019-04-13 18:40:09 +0200

    Add post request to api

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -14,26 +14,30 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TodoService {
-  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
   todosLimit = '?_limit=5';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   // Get Todos
-  getTodos():Observable<Todo[]> {
+  getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
   // Delete Todo
-    deleteTodo(todo:Todo):Observable<Todo> {
-      const url = `${this.todosUrl}/${todo.id}`;
-      return this.http.delete<Todo>(url, httpOptions);
-    }
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
 
+  // Add Todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+  
   // Toggle Completed
-  toggleCompleted(todo: Todo):Observable<any> {
+  toggleCompleted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`
     return this.http.put(url, todo, httpOptions)
   }
-
 }
