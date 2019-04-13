a423d5b63e175fb97adbc513ce8e43bdbdd60fbd
realbenfica <alexgtr84@gmail.com>
2019-04-13 17:35:24 +0200

    Add get request inside service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id:1,
-        title: "Todo One",
-        completed: false
-      },{
-        id:2,
-        title: "Todo Two",
-        completed: false
-      },
-      {
-        id:3,
-        title: "Todo Three",
-        completed: false
-      }
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
-  
+
 }
