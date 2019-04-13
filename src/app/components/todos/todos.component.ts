8156e10cf89bf69471c0fb6d48dcd709aa770d5c
realbenfica <alexgtr84@gmail.com>
2019-04-13 17:16:15 +0200

    Add my first service with data

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../models/Todo'
+import { TodoService } from '../../services/todo.service';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -9,26 +10,10 @@ import { Todo } from '../../models/Todo'
 export class TodosComponent implements OnInit {
   todos:Todo[];
 
-  constructor() { }
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
-    this.todos = [
-      {
-        id:1,
-        title: "Todo One",
-        completed: false
-      },{
-        id:2,
-        title: "Todo Two",
-        completed: false
-      },
-      {
-        id:3,
-        title: "Todo Three",
-        completed: false
-      }
-
-    ]
+    this.todos = this.todoService.getTodos();
   }
 
 }
