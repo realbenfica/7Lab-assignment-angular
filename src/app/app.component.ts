71c43875051daf8aab4653f05bfa46428b433dfb
realbenfica <alexgtr84@gmail.com>
2019-04-13 16:11:21 +0200

    Add new component and get it ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  what = 'Awesomeness';
+  name: string = 'Alex';
+
+  constructor() {
+    this.changeName("John")
+  }
+
+  changeName(name: string): void {
+    this.name = name
+  }
 }
