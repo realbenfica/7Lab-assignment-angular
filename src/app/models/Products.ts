448d18bfd44ebc2dff543863a7111dfe29b6736d
realbenfica <alexgtr84@gmail.com>
2019-04-16 10:18:35 +0200

    Add final assignment form

--- src/app/models/Products.ts
+++ src/app/models/Products.ts
@@ -3,18 +3,18 @@ import { NumberSymbol } from '@angular/common';
 export class Products {
     id: number;
     name: string;
-    description: string;
-    price: string;
-    stock_id: number;
-    type_id: number;
-    user_id: number;
-    created_at: string;
-    updated_at: string;
-    stock: {
+    description?: string;
+    price?: string;
+    stock_id?: number;
+    type_id?: number;
+    user_id?: number;
+    created_at?: string;
+    updated_at?: string;
+    stock?: {
         id: number;
         name: string;
     }
-    type: {
+    type?: {
         id: number;
         name: string;
         user_id: number;
